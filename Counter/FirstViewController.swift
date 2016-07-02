14e7d3f205782d640279d2911fc78865440c0df5
sadczi <dsadka@wi.zut.edu.pl>
2016-07-02 13:08:41 +0200

    Just made some fresh starting

--- Counter/FirstViewController.swift
+++ Counter/FirstViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
